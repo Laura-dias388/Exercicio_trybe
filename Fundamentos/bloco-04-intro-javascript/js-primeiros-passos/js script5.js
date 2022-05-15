389544a6436e45032f3a21c36edda91c75bfed38
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-05-15 04:38:01 -0300

    Exercicios de javaScript

---
+++ Fundamentos/bloco-04-intro-javascript/js-primeiros-passos/js script5.js
@@ -0,0 +1,10 @@
+//maior valor
+
+const a = 20;
+const b = 80;
+
+if ( a > b ){
+    console.log("O maior valor é :'a'");
+} else { 
+    console.log("O maior valor é:'b'");
+};
---
+++ Fundamentos/bloco-04-intro-javascript/js-primeiros-passos/js script7.js
@@ -0,0 +1,12 @@
+//numero negativo
+
+const numero =8;
+
+if(numero < 0){
+    console.log('Negativo');
+} else if(numero > 0){
+    console.log('Positivo');
+} else {
+    console.log('Zero')
+}
+ 
