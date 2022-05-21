ae45d609d95826715ff30e737a63b0ef1356d826
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-05-21 13:11:58 -0300

    exercicio 5.2

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercises5_2/script.js
@@ -0,0 +1,8 @@
+let container = document.getElementById('title');
+
+let newDiv = document.createElement('div');
+console.log(newDiv);
+
+newDiv.className = 'green';
+
+container.appendChild(newDiv);
