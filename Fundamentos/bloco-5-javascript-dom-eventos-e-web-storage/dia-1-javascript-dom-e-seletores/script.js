c603525ea280043abc5c0878eb30a7176d579e94
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-05-22 03:19:45 -0300

    exercicios do dia 5.1

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "black";
+
+    let mudarCor = document.querySelector('p').style.background = 'lightblue';
+    let newColor = document.querySelectorAll('.paragraph')[0].style.border = '8px solid blue';
+
+    let test = document.querySelector("subtitle")[0].style.background = '8px solid green';
+
