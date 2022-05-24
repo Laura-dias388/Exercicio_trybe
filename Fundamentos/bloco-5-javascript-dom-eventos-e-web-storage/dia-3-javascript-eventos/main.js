019c60e23b474645a852fdb59d759119c2973b85
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-05-24 15:11:42 -0300

    Exercicios eventos

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -7,31 +7,41 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+//Resposta 1.2 posição do .tech esta negativa(-20px);
+
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
+  function clickLi(){
+      
+  }
+  let ulElemento = document.getElementsByClassName('container').childNode; 
+  
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+let mouseOverLeaveButton = document.getElementById(myWebpage).style.backgroundColor = "red";
 
 // Segue abaixo um exemplo do uso de event.target:
 
-
 function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
+
+  // O Event é passado como um parâmetro para a função.
+  
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
-
 firstLi.addEventListener('dblclick', resetText);
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
