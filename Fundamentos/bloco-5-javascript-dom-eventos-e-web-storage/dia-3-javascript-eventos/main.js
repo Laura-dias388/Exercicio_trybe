ff1526d8034ad1d7b89c9d3a7b0938875ede747b
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-05-24 23:30:05 -0300

    Exercicio 5.3

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -5,45 +5,32 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
 
-// 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-//Resposta 1.2 posição do .tech esta negativa(-20px);
-
-
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-  function clickLi(){
-      
-  }
-  let ulElemento = document.getElementsByClassName('container').childNode; 
-  
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-
-
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portfólio?
-
-
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-let mouseOverLeaveButton = document.getElementById(myWebpage).style.backgroundColor = "red";
-
-// Segue abaixo um exemplo do uso de event.target:
-
 function resetText(event) {
-  event.target.innerText = 'Opção reiniciada';
-
-  // O Event é passado como um parâmetro para a função.
-  
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+  if(this.innerText === 'Opção reiniciada'){
+    event.target.innerText = 'Aqui está a primeira tecnologia que mais gostei.';
+  } else {
+    event.target.innerText = 'Opção reiniciada';
+  }
 }
-firstLi.addEventListener('dblclick', resetText);
+  firstLi.addEventListener('dblclick', resetText);
+  myWebpage.addEventListener('mouseover',changeColor);
+  firstLi.addEventListener('click', techChanger);
+  secondLi.addEventListener('click', techChanger);
+  thirdLi.addEventListener('click', techChanger);
+ 
+  function changeColor (event){
+    event.target.style.color = 'yellow';
+   
+  }
+   
 
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+   function techChanger(event){
+       
+    let resp = document.getElementsByClassName('tech');
+    resp[0].classList.remove('tech');
+    this.classList.add('tech');
+    
+  }
+
+   
+  
