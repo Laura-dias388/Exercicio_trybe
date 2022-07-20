4c5c82dec04a4598e9b72ac9c94db26055b53668
Laura Dias <ldiasvitaltoledo8@gmail.com>
2022-07-20 02:21:06 -0300

    Exercicio da aula ao vivo 10.1

--- Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my_app/src/App.js
+++ Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my_app/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
-function App() {
+/* function App() {// Componente funcional: Função que retorna um jsx;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá Mundo!!</h1>
     </div>
-  );
+  ) */
+  // classes conseguem agrupar um conjunto de propriredades em conjuntos de métodos;
+  // para extender o que a classe herda usa-se o extends;
+  // E o nome da classe que vamos extender: Component;
+
+  class App extends Component {
+    render() {// por trás dos panos o react entende que essa função é responsável por renderizar o conteúdo; Responsável por retornar o jsx;
+      return (
+        <HelloWorld />
+    );
+  }  
 }
 
 export default App;
---
+++ Front_end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my_app/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HelloWorld extends Component {
+
+    render() {
+        return (// o que for retornar deve estar dentro de um container;
+            <div>
+                <h1>Star Wars</h1>    
+            </div>   
+        ); 
+    }
+}
+
+export default HelloWorld;
