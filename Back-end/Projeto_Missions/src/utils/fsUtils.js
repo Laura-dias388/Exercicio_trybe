669eff99432d955d65b72d543ed12633c3b2a019
Laura-dias388 <ldiasvitaltoledo8@gmail.com>
2022-11-02 15:13:50 -0300

    Processo concluído

--- Back-end/Projeto_Missions/src/utils/fsUtils.js
+++ Back-end/Projeto_Missions/src/utils/fsUtils.js
@@ -1,19 +1,35 @@
 const fs = require('fs').promises; // de todas as versões desse arquivo eu quero usar a promisse por que é a mais performática;
-const path = require('path');
+const path = require('path');// lib path para resolver problemas de localização(__dirname);
+
+const MISSION_DATA_PATH = '../../data/missions.json';
 
 async function readMissionsData() {
-  try {
-    const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'));
-    const mission = JSON.parse(data);
-    console.log(mission);
-    return mission;
+  try { // tratamento de excessão
+    const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
+    const missions = JSON.parse(data);
+    console.log(missions);
+    return missions;
   } catch (error) {
     console.error(`Erro na leitura do arquivo: ${error}`);
   }
 }
 
-readMissionsData();
+async function writeNewMissionData(newMission) {
+  try {
+    const oldMissions = await readMissionsData();
+    const allMissions = JSON.stringify([
+      ...oldMissions, 
+      { id: Date.now(), 
+      ...newMission,
+    }]);
+
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions);
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+  }
+}
 
 module.exports = {
   readMissionsData,
-}
+  writeNewMissionData,
+};
